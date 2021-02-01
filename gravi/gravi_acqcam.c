b8edd981905d7e4acfab88275903110fbde0f4ed
slacour <slacour@318c8218-2dab-440d-9263-6f70bac63e5b>
2021-02-01 10:49:02 +0000

    adding new values to the QC, adding imprint to mean img, and last, adding check on number of frames (PIPE-9367)
    git-svn-id: http://svnhq2.hq.eso.org/p2/branches/Pipelines/gravity/gravity_dev/gravityp@260601 318c8218-2dab-440d-9263-6f70bac63e5b

--- gravi/gravi_acqcam.c
+++ gravi/gravi_acqcam.c
@@ -1956,7 +1956,7 @@ cpl_error_code gravi_acqcam_get_diode_ref_v2 (cpl_propertylist * header,
     
     cpl_size nrow = cpl_array_get_size(good_frames);
     if (fabs(cpl_array_get_mean(good_frames)*nrow -nrow_on) > 1e-2)
-        cpl_msg_error (cpl_func, "Ratio of blinking frames different %f from %i",cpl_array_get_mean(good_frames)*nrow,nrow_on);
+        cpl_msg_error (cpl_func, "Ratio of blinking frames different %f from %lli",cpl_array_get_mean(good_frames)*nrow,nrow_on);
     
     for (int tel = 0 ; tel < GRAVI_SPOT_NTEL; tel++)
     {
