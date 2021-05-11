8181d168507d9b1269d849ee9ffea974aac5b4b1
slacour <slacour@318c8218-2dab-440d-9263-6f70bac63e5b>
2021-05-11 07:47:05 +0000

    update of definition of the use-met-zero parameter (to make it clearer)
    git-svn-id: http://svnhq2.hq.eso.org/p2/branches/Pipelines/gravity/gravity_dev/gravityp@264272 318c8218-2dab-440d-9263-6f70bac63e5b

--- gravi/gravi_dfs.c
+++ gravi/gravi_dfs.c
@@ -704,8 +704,8 @@ cpl_error_code gravi_parameter_add_rejection (cpl_parameterlist *self, int isCal
     
     /* Flag to activate metrology zero calculation */
     p = cpl_parameter_new_value ("gravity.signal.use-met-zero", CPL_TYPE_BOOL,
-                                 "Flag to activate metrology zero calculation in OPD_DISP.\n "
-                                 "If disabled all metrology zeroes are set to zero.",
+                                 "Flag to add a constant value to OPD_DISP.\n "
+                                 "This constant value is taken from the header.",
                                  "gravity.signal", FALSE);
     cpl_parameter_set_alias (p, CPL_PARAMETER_MODE_CLI, "use-met-zero");
     cpl_parameter_disable (p, CPL_PARAMETER_MODE_ENV);
